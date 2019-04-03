f5645fd2e12fffd25587de5bdd01a004f9653eb8
pnghiaacoustic <pnghiaacoustic@gmail.com>
2019-04-03 13:45:39 +0700

    Create

--- Tic_BigSchool/Controllers/CoursesController.cs
+++ Tic_BigSchool/Controllers/CoursesController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tic_BigSchool.Models;
 
 namespace Tic_BigSchool.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         // GET: Courses
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModels
+            {
+                Categories = _dbContext.CateGories.ToList()
+            };
+            return View(viewModel);
         }
     }
 }
---
+++ Tic_BigSchool/Models/CourseViewModels.cs
@@ -0,0 +1,21 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tic_BigSchool.Models
+{
+    public class CourseViewModels
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<CateGory> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time)); 
+        }
+    }
+}
